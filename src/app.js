36099280bfbc3e75f80c2faac2b680bc847c9158
Alexandre <alexandrefsoficial@gmail.com>
2023-01-05 23:19:15 -0300

    sign up validation bug fix /tweet validation added

--- src/app.js
+++ src/app.js
@@ -11,30 +11,45 @@ const users = []
 
 app.post("/sign-up", (req, res) => {
     const signUpData = req.body;
-    const signUpKeys = Object.keys(signUpData);
     //username or avatar doesn't exist
+    const signUpKeys = Object.keys(signUpData);
     if((signUpKeys.indexOf("username") === (-1)) || (signUpKeys.indexOf("avatar") === (-1))){
         res.sendStatus(400)
     }
-    //username or avatar empty
+    //username or avatar is empty
     if(signUpData.username.length === 0 || signUpData.avatar.length === 0){
         res.sendStatus(400)
     }
     //username or avatar is not string
-    if(typeof signUpData.avatar === "string" || typeof signUpData.username === "string"){
+    if(typeof signUpData.avatar !== "string" || typeof signUpData.username !== "string"){
         res.sendStatus(400)
     }
+    //posting user in server memory
     users.push(signUpData);
     res.send("OK");
 })
 
 app.post("/tweets", (req, res) => {
     const tweetData = req.body;
+    //tweet doesn't exist
+    const tweetKeys = Object.keys(tweetData);
+    if((tweetKeys.indexOf("tweet") === (-1))){
+        res.sendStatus(400);
+    }
+    //tweet is empty
+    if(tweetData.tweet.length === 0){
+        res.sendStatus(400)
+    }
+    //tweet is not string
+    if(typeof tweetData.tweet !== "string"){
+        res.sendStatus(400)
+    }
+    //posting tweet in server memory
     users.forEach(item => {
         if (item.username.includes(tweetData.username)) {
-            tweets.unshift({ ...tweetData, avatar: item.avatar })
-            console.log(tweets)
-            res.send("OK")
+            tweets.unshift({ ...tweetData, avatar: item.avatar });
+            console.log(tweets);
+            res.send("OK");
         }
     })
     res.send("UNAUTHORIZED")
