a2e96d81f1a6b199c577dde6af919b581501f975
Alexandre <alexandrefsoficial@gmail.com>
2023-01-05 21:52:20 -0300

    emptied tweets before api starts

--- src/app.js
+++ src/app.js
@@ -6,19 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 
-const tweets = [
-    {
-        username: "bobesponja",
-        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-        tweet: "eu amo o hub"
-    },
-    {
-        username: "bobesponja",
-        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-        tweet: "eu amo o hub"
-    }
-]
-
+const tweets = []
 const users = []
 
 app.get("/tweets", (req, res) => {
